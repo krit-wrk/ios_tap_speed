667211a5d1835d33bcc67fdaeef0e6b8fcbe3b4a
Krit <krit.wrk@gmail.com>
2021-10-29 20:00:11 +0700

    init

---
+++ ios_tap_speed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_tap_speed
+//
+//  Created by Ping Krittanon on 29/10/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_tap_speed/ios_tap_speedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_tap_speedApp.swift
+//  ios_tap_speed
+//
+//  Created by Ping Krittanon on 29/10/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct ios_tap_speedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
